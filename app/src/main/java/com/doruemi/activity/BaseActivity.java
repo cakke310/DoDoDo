9aa3c86feda40415033196ad8a659fd83f965dd1
cupcake.xu <cupcake.x>
2016-08-19 17:01:49 +0800

    sync

--- app/src/main/java/com/doruemi/activity/BaseActivity.java
+++ app/src/main/java/com/doruemi/activity/BaseActivity.java
@@ -1,7 +1,50 @@
 package com.doruemi.activity;
 
+import android.app.Activity;
+import android.os.Bundle;
+
+import butterknife.ButterKnife;
+
 /**
  * Created by Administrator on 2016/8/17.
  */
-public class BaseActivity {
+public abstract class BaseActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(setContentViewId());
+        ButterKnife.bind(this);
+        initView();
+        initData();
+        initListener();
+    }
+
+    protected abstract void initListener();
+
+    protected abstract void initData();
+
+    protected abstract void initView();
+
+    public abstract int setContentViewId();
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ButterKnife.unbind(this);
+    }
 }
--- app/src/main/java/com/doruemi/activity/MainActivity.java
+++ app/src/main/java/com/doruemi/activity/MainActivity.java
@@ -1,16 +1,52 @@
 package com.doruemi.activity;
 
-import android.app.Activity;
+
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 
 import com.doruemi.R;
+import com.doruemi.fragment.BaseFragment;
+import com.doruemi.fragment.BtnFragmentFactory;
 
+import java.util.ArrayList;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
+
+    private ArrayList<BaseFragment> fragments;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        initFragments();
+        initData();
+    }
+
+
+    private void initData() {
+        changeFragment(0);
+    }
+
+    private void initFragments() {
+        fragments = new ArrayList<>();
+        fragments.add(BtnFragmentFactory.createFragment(BtnFragmentFactory.TAB_MAIN));
+    }
+
+    private void initView() {
+    }
+
+
+
+    public void changeFragment(int i){
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction beginTransaction = fm.beginTransaction();
+        BaseFragment fragment = fragments.get(i);
+        beginTransaction.replace(R.id.fl_container,fragment);
+        beginTransaction.commit();
     }
 }
