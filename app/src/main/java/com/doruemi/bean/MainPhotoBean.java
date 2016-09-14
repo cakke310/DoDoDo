e51b255509d0b73f2b279a1b24a68c01baaa52e7
cupcake.xu <cupcake.x>
2016-09-14 17:49:01 +0800

    sync

---
+++ app/src/main/java/com/doruemi/bean/MainPhotoBean.java
@@ -0,0 +1,421 @@
+package com.doruemi.bean;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2016/9/14.
+ */
+public class MainPhotoBean {
+
+    /**
+     * ErrorMsg :
+     * code : 1
+     * list : [{"id":"668058","userid":"6192","username":"张旺乔","content":"遮羞","address":"","imgurl":"width_640/upload/pics/201609/6/6192/1473827722.jpg","height":"340","laud":"1","comment":"0","addtime":"1473827722","following":"0","haslaud":"0","isdaren":"1","isdownload":"0","comments":[]},{"id":"668049","userid":"71","username":"尿姑娘","content":"🐯#简单生活","address":"","imgurl":"width_640/upload/pics/201609/0/71/1473825968.jpg","height":"641","laud":"9","comment":"0","addtime":"1473825968","following":"0","haslaud":"0","isdaren":"1","isdownload":"0","comments":[]},{"id":"668045","userid":"62170","username":"周咖喱","content":"","address":"","imgurl":"width_640/upload/pics/201609/62/62170/1473825026.jpg","height":"640","laud":"6","comment":"7","addtime":"1473825026","following":"0","haslaud":"0","isdaren":"1","isdownload":"0","comments":[{"cid":"370475","article_id":"668045","pid":"0","userid":"569","username":"短发控","content":"卧槽，想把痣抠掉🙄🙄","addtime":"1473832166"},{"cid":"370469","article_id":"668045","pid":"0","userid":"273","username":"大B哥617","content":"两女咯","addtime":"1473828196"},{"cid":"370468","article_id":"668045","pid":"0","userid":"7610","username":"CeCe","content":"强无敌","addtime":"1473827160"}]},{"id":"668041","userid":"53117","username":"J导","content":"动起来 就现在","address":"","imgurl":"width_640/upload/pics/201609/53/53117/1473824080.jpg","height":"480","laud":"3","comment":"0","addtime":"1473824080","following":"0","haslaud":"0","isdaren":"1","isdownload":"1","comments":[]},{"id":"668032","userid":"18792","username":"列马革","content":"#城市#情绪","address":"","imgurl":"width_640/upload/pics/201609/18/18792/1473823532.jpg","height":"404","laud":"3","comment":"0","addtime":"1473823532","following":"0","haslaud":"0","isdaren":"1","isdownload":"1","comments":[]},{"id":"668026","userid":"11939","username":"多多与针针","content":"","address":"","imgurl":"width_640/upload/pics/201609/11/11939/1473823118.jpg","height":"640","laud":"1","comment":"0","addtime":"1473823118","following":"0","haslaud":"0","isdaren":"1","isdownload":"0","comments":[]},{"id":"667996","userid":"13348","username":"苦楽先生","content":"#丢掉书本上街去#浪摄流","address":"","imgurl":"width_640/upload/pics/201609/13/13348/1473822041.jpg","height":"640","laud":"1","comment":"0","addtime":"1473822041","following":"0","haslaud":"0","isdaren":"1","isdownload":"0","comments":[]},{"id":"667995","userid":"30404","username":"江上蓬蒿人","content":"#手机摄影#南京","address":"","imgurl":"width_640/upload/pics/201609/30/30404/1473821989.jpg","height":"640","laud":"1","comment":"0","addtime":"1473821989","following":"0","haslaud":"0","isdaren":"1","isdownload":"1","comments":[]},{"id":"667980","userid":"79818","username":"牙疼服务器","content":"-","address":"","imgurl":"width_640/upload/pics/201609/79/79818/1473821058.jpg","height":"594","laud":"11","comment":"0","addtime":"1473821058","following":"0","haslaud":"0","isdaren":"1","isdownload":"1","comments":[]},{"id":"667977","userid":"6552","username":"夏风","content":"就在 梦里见过你\n#我的照片有情绪","address":"","imgurl":"width_640/upload/pics/201609/6/6552/1473820507.jpg","height":"427","laud":"12","comment":"0","addtime":"1473820507","following":"0","haslaud":"0","isdaren":"1","isdownload":"1","comments":[]},{"id":"667976","userid":"82753","username":"CD1984","content":"","address":"","imgurl":"width_640/upload/pics/201609/82/82753/1473820126.jpg","height":"776","laud":"7","comment":"0","addtime":"1473820126","following":"0","haslaud":"0","isdaren":"1","isdownload":"1","comments":[]},{"id":"667975","userid":"24","username":"Hipstamatic","content":"#简单生活#秋天#Iphonephoto","address":"","imgurl":"width_640/upload/pics/201609/0/24/1473819862.jpg","height":"480","laud":"2","comment":"0","addtime":"1473819862","following":"0","haslaud":"0","isdaren":"1","isdownload":"1","comments":[]},{"id":"667974","userid":"43","username":"gastychai","content":"#简单生活","address":"","imgurl":"width_640/upload/pics/201609/0/43/1473819553.jpg","height":"854","laud":"1","comment":"0","addtime":"1473819553","following":"0","haslaud":"0","isdaren":"1","isdownload":"0","comments":[]},{"id":"667973","userid":"65","username":"PAPATA","content":"#简单生活","address":"","imgurl":"width_640/upload/pics/201609/0/65/1473819536.jpg","height":"800","laud":"3","comment":"0","addtime":"1473819536","following":"0","haslaud":"0","isdaren":"1","isdownload":"1","comments":[]},{"id":"667969","userid":"75736","username":"whyseeimage","content":"\\\\\\\\\\\\#光影#城市#街头#街头摄影#建筑#手机摄影#摄影","address":"","imgurl":"width_640/upload/pics/201609/75/75736/1473819090.jpg","height":"854","laud":"5","comment":"0","addtime":"1473819091","following":"0","haslaud":"0","isdaren":"1","isdownload":"0","comments":[]}]
+     * flag : 1473833334
+     * start : 15
+     * steps : 15
+     * matchlist : [{"id":"134","title":"晒简单生活，赢上海简单生活音乐节门票！！","imgurl":"upload/evnets/2016/09/13/1473743515.png","eventtype":"6"},{"id":"133","title":"【征稿标签：黄】更多色，要黄图","imgurl":"upload/evnets/2016/09/06/1473152727.jpg","eventtype":"3"},{"id":"131","title":"感受天空的色彩，拍下你眼里的世界！！","imgurl":"upload/evnets/2016/08/15/1471236606.png","eventtype":"3"}]
+     * matchhis : 1
+     * unfollowing : {"userid":"1791","id":"653857","username":"Cher","isdaren":"1","content":"","address":"","imgurl":"upload/pics/201608/1/1791/1471705687.jpg","height":"480","laud":"6","comment":"0","addtime":"1471705687"}
+     */
+
+    private String ErrorMsg;
+    private String code;
+    private int flag;
+    private int start;
+    private int steps;
+    private int matchhis;
+    /**
+     * userid : 1791
+     * id : 653857
+     * username : Cher
+     * isdaren : 1
+     * content :
+     * address :
+     * imgurl : upload/pics/201608/1/1791/1471705687.jpg
+     * height : 480
+     * laud : 6
+     * comment : 0
+     * addtime : 1471705687
+     */
+
+    private UnfollowingEntity unfollowing;
+    /**
+     * id : 668058
+     * userid : 6192
+     * username : 张旺乔
+     * content : 遮羞
+     * address :
+     * imgurl : width_640/upload/pics/201609/6/6192/1473827722.jpg
+     * height : 340
+     * laud : 1
+     * comment : 0
+     * addtime : 1473827722
+     * following : 0
+     * haslaud : 0
+     * isdaren : 1
+     * isdownload : 0
+     * comments : []
+     */
+
+    private List<ListEntity> list;
+    /**
+     * id : 134
+     * title : 晒简单生活，赢上海简单生活音乐节门票！！
+     * imgurl : upload/evnets/2016/09/13/1473743515.png
+     * eventtype : 6
+     */
+
+    private List<MatchlistEntity> matchlist;
+
+    public String getErrorMsg() {
+        return ErrorMsg;
+    }
+
+    public void setErrorMsg(String ErrorMsg) {
+        this.ErrorMsg = ErrorMsg;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public int getFlag() {
+        return flag;
+    }
+
+    public void setFlag(int flag) {
+        this.flag = flag;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public void setStart(int start) {
+        this.start = start;
+    }
+
+    public int getSteps() {
+        return steps;
+    }
+
+    public void setSteps(int steps) {
+        this.steps = steps;
+    }
+
+    public int getMatchhis() {
+        return matchhis;
+    }
+
+    public void setMatchhis(int matchhis) {
+        this.matchhis = matchhis;
+    }
+
+    public UnfollowingEntity getUnfollowing() {
+        return unfollowing;
+    }
+
+    public void setUnfollowing(UnfollowingEntity unfollowing) {
+        this.unfollowing = unfollowing;
+    }
+
+    public List<ListEntity> getList() {
+        return list;
+    }
+
+    public void setList(List<ListEntity> list) {
+        this.list = list;
+    }
+
+    public List<MatchlistEntity> getMatchlist() {
+        return matchlist;
+    }
+
+    public void setMatchlist(List<MatchlistEntity> matchlist) {
+        this.matchlist = matchlist;
+    }
+
+    public static class UnfollowingEntity {
+        private String userid;
+        private String id;
+        private String username;
+        private String isdaren;
+        private String content;
+        private String address;
+        private String imgurl;
+        private String height;
+        private String laud;
+        private String comment;
+        private String addtime;
+
+        public String getUserid() {
+            return userid;
+        }
+
+        public void setUserid(String userid) {
+            this.userid = userid;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getIsdaren() {
+            return isdaren;
+        }
+
+        public void setIsdaren(String isdaren) {
+            this.isdaren = isdaren;
+        }
+
+        public String getContent() {
+            return content;
+        }
+
+        public void setContent(String content) {
+            this.content = content;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+
+        public String getImgurl() {
+            return imgurl;
+        }
+
+        public void setImgurl(String imgurl) {
+            this.imgurl = imgurl;
+        }
+
+        public String getHeight() {
+            return height;
+        }
+
+        public void setHeight(String height) {
+            this.height = height;
+        }
+
+        public String getLaud() {
+            return laud;
+        }
+
+        public void setLaud(String laud) {
+            this.laud = laud;
+        }
+
+        public String getComment() {
+            return comment;
+        }
+
+        public void setComment(String comment) {
+            this.comment = comment;
+        }
+
+        public String getAddtime() {
+            return addtime;
+        }
+
+        public void setAddtime(String addtime) {
+            this.addtime = addtime;
+        }
+    }
+
+    public static class ListEntity {
+        private String id;
+        private String userid;
+        private String username;
+        private String content;
+        private String address;
+        private String imgurl;
+        private String height;
+        private String laud;
+        private String comment;
+        private String addtime;
+        private String following;
+        private String haslaud;
+        private String isdaren;
+        private String isdownload;
+        private List<?> comments;
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getUserid() {
+            return userid;
+        }
+
+        public void setUserid(String userid) {
+            this.userid = userid;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getContent() {
+            return content;
+        }
+
+        public void setContent(String content) {
+            this.content = content;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+
+        public String getImgurl() {
+            return imgurl;
+        }
+
+        public void setImgurl(String imgurl) {
+            this.imgurl = imgurl;
+        }
+
+        public String getHeight() {
+            return height;
+        }
+
+        public void setHeight(String height) {
+            this.height = height;
+        }
+
+        public String getLaud() {
+            return laud;
+        }
+
+        public void setLaud(String laud) {
+            this.laud = laud;
+        }
+
+        public String getComment() {
+            return comment;
+        }
+
+        public void setComment(String comment) {
+            this.comment = comment;
+        }
+
+        public String getAddtime() {
+            return addtime;
+        }
+
+        public void setAddtime(String addtime) {
+            this.addtime = addtime;
+        }
+
+        public String getFollowing() {
+            return following;
+        }
+
+        public void setFollowing(String following) {
+            this.following = following;
+        }
+
+        public String getHaslaud() {
+            return haslaud;
+        }
+
+        public void setHaslaud(String haslaud) {
+            this.haslaud = haslaud;
+        }
+
+        public String getIsdaren() {
+            return isdaren;
+        }
+
+        public void setIsdaren(String isdaren) {
+            this.isdaren = isdaren;
+        }
+
+        public String getIsdownload() {
+            return isdownload;
+        }
+
+        public void setIsdownload(String isdownload) {
+            this.isdownload = isdownload;
+        }
+
+        public List<?> getComments() {
+            return comments;
+        }
+
+        public void setComments(List<?> comments) {
+            this.comments = comments;
+        }
+    }
+
+    public static class MatchlistEntity {
+        private String id;
+        private String title;
+        private String imgurl;
+        private String eventtype;
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getImgurl() {
+            return imgurl;
+        }
+
+        public void setImgurl(String imgurl) {
+            this.imgurl = imgurl;
+        }
+
+        public String getEventtype() {
+            return eventtype;
+        }
+
+        public void setEventtype(String eventtype) {
+            this.eventtype = eventtype;
+        }
+    }
+}
